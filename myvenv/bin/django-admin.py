ebeccd12d97fdd85e418020e0390f934ae94fc5c
cristian pezoa <cristianpezoa@MacBook-Pro-de-cristian.local>
2020-07-16 13:43:56 -0400

    primer commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cristianpezoa/Documents/PORTAFOLIO/Semestral01_07.06_v2/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
